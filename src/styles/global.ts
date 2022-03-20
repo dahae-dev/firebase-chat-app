0a01de95db1165861d65e28a8edaeadacaa75b43
dahae.kim <dahae.kim@tridge.com>
2022-03-20 18:46:46 +0900

    Fix width for desktop viewport

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -10,6 +10,12 @@ const GlobalStyle = createGlobalStyle`
     background-color: white;
     font-family: 'Apple SD Gothic', sans-serif;
     height: 100%;
+    @media screen and (min-width: 1080px) {
+      position: absolute;
+      left: 50%;
+      transform: translateX(-50%);
+      width: 450px;
+    }
   }
   a {
     text-decoration: none;
